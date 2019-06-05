1026dcc0ea6c35f9c142cbacca4c93b09f9fc5b8
seuba <49449750+seuba@users.noreply.github.com>
2019-06-05 13:49:29 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -58,7 +58,7 @@ Http.onreadystatechange = (e) => {
 	/**/
 	//var url = 'https://pub.s10.exacttarget.com/1r4ckkca1cs?mapskey=' + mapsKey +
 	//	'&link=' + link;
-	sdk.setContent('<p>' + mapsKey + '</p>');
+	sdk.setContent('<a href="aaa">aaaaa</a>');
 	sdk.setData({
 		address: address,
 		width: width,
