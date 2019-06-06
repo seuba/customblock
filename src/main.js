b553c33f6fc4db6f8d5d957e17d1f0c0f2b09c29
seuba <49449750+seuba@users.noreply.github.com>
2019-06-06 21:17:46 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -54,7 +54,7 @@ Http.onreadystatechange = (e) => {
   console.log(Http.responseText)
   
   	const Http2 = new XMLHttpRequest();
-	const url2='https://www.cangureo.es/ajaxr.php';
+	const url2='https://www.cangureo.es/ajaxr.php?link=' + link;
 	Http2.open("GET", url2);
 	Http2.send();
      Http2.onreadystatechange = (e) => {
